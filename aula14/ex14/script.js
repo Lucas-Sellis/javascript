3116dedc2b92d1e188d4682960e62c2ca79e652d
Lucas Sellis <lucas.sellis@gmail.com>
2025-01-20 08:23:42 -0300

    f

--- aula14/ex14/script.js
+++ aula14/ex14/script.js
@@ -11,6 +11,11 @@ function contar() {
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo Inválido! Considerando PASSO 1')
+            p = 1 
+        }
+
         if (i < f){
             //Contagem Crescente
             for(let c = i; c <= f; c+= p) {
